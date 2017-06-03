39655ca06fe7e37e72d1ac4a6afa894f1343b53b
Nikita Chursin <nchursin@aquivalabs.com>
2017-06-03 13:42:15 +0300

    default_browser setting handler

--- unity3d_reference.py
+++ unity3d_reference.py
@@ -21,11 +21,23 @@ def on_cancel():
 def SearchFor(data):
 	defaults = sublime.load_settings('Default.sublime-settings')
 	settings = sublime.load_settings('Unity3DReference.sublime-settings')
+	platform = sublime.platform()
 
 	use_local_url = settings.get('use_local_url', defaults.get('use_local_url'))
 	local_url = settings.get('local_url', defaults.get('local_url'))
 	default_url = 'http://docs.unity3d.com/ScriptReference/30_search.html'
 
+	default_browser = settings.get('default_browser', defaults.get('default_browser'))
+	if default_browser:
+		if platform == 'osx':
+			browser_path = 'open -a ' + "\"%s\"" % default_browser
+		else:
+			browser_path = "\"%s\"" % default_browser
+		browser_path += ' %s'
+		open_new_tab = webbrowser.get(browser_path).open_new_tab
+	else:
+		open_new_tab = webbrowser.open_new_tab
+
 	url = use_local_url and local_url or default_url
 
-	webbrowser.open_new_tab(url + '?q={0}'.format(data))
+	open_new_tab(url + '?q={0}'.format(data))
